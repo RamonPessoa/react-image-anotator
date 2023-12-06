47321705487196ae75663fe6136e6229e1517413
RamonPessoa <reimund.programmer@gmail.com>
2023-12-06 10:19:08 -0300

    chore: configure some aliases for better import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
+const root = path.resolve(__dirname, "src");
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@components": path.resolve(root, "components"),
+      "@assets": path.resolve(root, "assets"),
+      "@domain": path.resolve(root, "domain"),
+    },
+  },
+});
